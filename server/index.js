485fdfd1977a985bf919540b9b48ab6d8463baa0
firdaoues <firdaouesmiraoui@gmail.com>
2022-06-18 21:20:57 +0200

    correction

--- server/index.js
+++ server/index.js
@@ -6,11 +6,10 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
+app.use("/posts", postRoutes);
 // mongodb cloud atlas
 const CONNECTION_URL =
   "mongodb+srv://memoriesproject:memoriesproject123@cluster0.5bgnw.mongodb.net/?retryWrites=true&w=majority";
