ce5a607362f3d32325fc87e22b73042b886f6bcb
firdaoues <firdaouesmiraoui@gmail.com>
2022-06-21 01:54:47 +0200

    CRUD

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -11,6 +11,7 @@ export const getPosts = () => async (dispatch) => {
     console.log(error.message);
   }
 };
+
 export const createPost = (post) =>async (dispatch)=> {
   try{
     const { data } = await api.createPost(post);
@@ -20,4 +21,30 @@ export const createPost = (post) =>async (dispatch)=> {
   }
 };
 
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
 
+    dispatch({ type: "UPDATE" , payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const deletePost = (id) => async(dispatch) => {
+  try{
+    await api.deletePost(id);
+    dispatch({ type: "DELETE" , payload: id});
+  } catch (error) {
+    console.log(error);
+  }
+}
+ export const likePost = (id) => async(dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: "UPDATE" , payload: data });
+
+  } catch (error) {
+    console.log(error);
+  }
+ }
