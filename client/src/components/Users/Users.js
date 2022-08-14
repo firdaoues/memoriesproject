631ebd84ded0b7a37c3cb1b03f0767146b4b3362
firdaoues <firdaouesmiraoui@gmail.com>
2022-08-14 21:48:41 +0200

    users

--- client/src/components/Users/Users.js
+++ client/src/components/Users/Users.js
@@ -1,12 +1,25 @@
 
 import React , { useEffect }from 'react'
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { getUsers} from "../../actions/posts";
+import useStyles from "./styles";
+
+const handleClick = async () => {
+}
+
 const Users = () => {
+  const classes = useStyles();
     const dispatch = useDispatch();
     const users = useSelector((state) => state.users);
     console.log(users);
@@ -16,17 +29,33 @@ const Users = () => {
       }, [dispatch])
   return (
     <>
-          
-   <div> </div>
-    { users.map((user) => (
+<TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+        <TableRow >
+            <TableCell className={classes.table}>Name</TableCell>
+            <TableCell >Email</TableCell>
+            </TableRow>
+        </TableHead>
+        <TableBody>
+        { users.map((user) => (
+         <>
+         <TableRow>
+        <TableCell key={user.id}
+   onClick={() => handleClick(user.id)}>
+         
+        {user.name}
         
-        <div>
-          <h1>{user.name}</h1>
-          <p>{user.email}</p>
-          
-          </div>
+      </TableCell>
+      <TableCell >{user.email}</TableCell>
+   </TableRow>
+      </>
       ))}
- 
+        </TableBody>
+        </Table>
+      </TableContainer>
+   
+   
     </>
   )
     }
